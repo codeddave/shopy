ff33bb02cb3e2e17daef110d9035280d1fad185e
codeddave <drizzzydave@gmail.com>
2022-03-28 22:02:11 +0100

    updated Product Screen

--- app/screens/ProductScreen.tsx
+++ app/screens/ProductScreen.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import data from "../../assets/data/products.json";
 
 const ProductScreen = () => {
-  return <div>ProductScreen</div>;
+  const [products, setProducts] = useState<{}[]>([]);
+  useEffect(() => {
+    setProducts(data);
+    return () => {
+      setProducts([]);
+    };
+  }, []);
+  return (
+    <View>
+      <Text>ProductScreen</Text>
+      <FlatList
+        data={products}
+        renderItem={({ item }: any) => <Text> {item.brand}</Text>}
+        keyExtractor={(item: any) => item.name}
+      />
+    </View>
+  );
 };
 
 export default ProductScreen;
