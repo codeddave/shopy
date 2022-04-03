41869536c5e6d450f636e75c85b508233de9f852
codeddave <drizzzydave@gmail.com>
2022-04-03 16:15:19 +0100

    styling updates, added native-base

--- app/screens/ProductScreen.tsx
+++ app/screens/ProductScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, View } from "react-native";
 import data from "../../assets/data/products.json";
 import ProductList from "../components/products/ProductList";
-
+import { Container, Input, HStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 const ProductScreen = () => {
   const [products, setProducts] = useState<{}[]>([]);
   useEffect(() => {
@@ -12,22 +13,42 @@ const ProductScreen = () => {
     };
   }, []);
   return (
-    <View
-      style={{
-        backgroundColor: "gainsboro",
-        paddingBottom: 30,
-      }}
-    >
-      <FlatList
-        scrollEnabled
-        numColumns={2}
-        data={products}
-        renderItem={({ item }: any) => (
-          <ProductList key={item.id} item={item} />
-        )}
-        keyExtractor={(item: any) => item.name}
+    <>
+      <Input
+        placeholder="Search"
+        width="100%"
+        variant="filled"
+        borderRadius="10"
+        py="1"
+        px="2"
+        borderWidth="0"
+        InputLeftElement={
+          <Icon
+            ml="2"
+            size="4"
+            color="gray.400"
+            as={<MaterialIcons name="search" />}
+          />
+        }
       />
-    </View>
+      <View
+        style={{
+          backgroundColor: "gainsboro",
+          paddingBottom: 30,
+          marginTop: 4,
+        }}
+      >
+        <FlatList
+          scrollEnabled
+          numColumns={2}
+          data={products}
+          renderItem={({ item }: any) => (
+            <ProductList key={item.id} item={item} />
+          )}
+          keyExtractor={(item: any) => item.name}
+        />
+      </View>
+    </>
   );
 };
 
