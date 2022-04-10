f816a85394033b8e7bafe2c68c517a8ed31e4d64
codeddave <drizzzydave@gmail.com>
2022-04-10 22:47:23 +0100

    building the app banner

---
+++ app/components/shared/Banner.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import Swiper from "react-native-swiper";
+
+const { width } = Dimensions.get("window");
+
+const Banner = () => {
+  const [bannerData, setBannerData] = useState<string[]>([]);
+
+  useEffect(() => {
+    setBannerData([
+      "https://images.vexels.com/media/users/3/126443/preview2/ff9af1e1edfa2c4a46c43b0c2040ce52-macbook-pro-touch-bar-banner.jpg",
+      "https://pbs.twimg.com/media/D7P_yLdX4AAvJWO.jpg",
+      "https://www.yardproduct.com/blog/wp-content/uploads/2016/01/gardening-banner.jpg",
+    ]);
+    return () => {
+      setBannerData([]);
+    };
+  }, []);
+  return <div>Banner</div>;
+};
+const styles = StyleSheet.create({});
+export default Banner;
