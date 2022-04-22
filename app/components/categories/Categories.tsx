209c9e8ccfe5b6fe5151880944cfc2bff61dc230
codeddave <drizzzydave@gmail.com>
2022-04-22 01:16:55 +0100

    updats to Categories section

--- app/components/categories/Categories.tsx
+++ app/components/categories/Categories.tsx
@@ -2,7 +2,19 @@ import { Badge, Box, Text } from "native-base";
 import React from "react";
 import { TouchableOpacity, StyleSheet, ScrollView } from "react-native";
 
-const Categories = () => {
+type Props = {
+  categories: any[];
+  categoryFilter: any;
+  productCategories: any;
+  active: boolean;
+  setActive: any;
+};
+const Categories = ({ setActive, categoryFilter, active }: Props) => {
+  const handleBadgeClick = (filter: string) => {
+    categoryFilter(filter);
+
+    setActive(!active);
+  };
   return (
     <ScrollView
       horizontal
@@ -10,10 +22,21 @@ const Categories = () => {
       style={{ backgroundColor: "#f2f2f2", marginTop: 10 }}
     >
       <Box>
-        <TouchableOpacity key={1}>
-          <Badge style={[styles.center, { margin: 5 }]}>
+        <TouchableOpacity
+          onPress={() => {
+            handleBadgeClick("all");
+          }}
+          key={1}
+        >
+          <Badge
+            style={[
+              styles.center,
+              { margin: 5 },
+              active ? styles.active : styles.inactive,
+            ]}
+          >
             <Text paddingBottom="4" color="white">
-              name
+              All
             </Text>
           </Badge>
         </TouchableOpacity>
@@ -23,9 +46,15 @@ const Categories = () => {
 };
 
 const styles = StyleSheet.create({
+  active: {
+    backgroundColor: "#03bafc",
+  },
   center: {
     justifyContent: "center",
     alignItems: "center",
+  },
+  inactive: {
+    backgroundColor: "#a0e1eb",
   },
 });
 
