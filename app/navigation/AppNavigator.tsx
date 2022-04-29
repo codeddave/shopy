663a7566fe3fb4e420e218ebf447fdd930e6e351
codeddave <drizzzydave@gmail.com>
2022-04-29 23:02:12 +0100

    updates

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,13 +1,36 @@
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" />
+      <Tab.Screen
+        name="Home"
+        component={() => <div></div>}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={() => <div></div>}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="cart-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
