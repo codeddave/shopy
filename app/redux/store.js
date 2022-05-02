549c5e701da5dc64de37d724f2e422c2d407df74
codeddave <drizzzydave@gmail.com>
2022-05-02 10:56:58 +0100

    update

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,12 +1,11 @@
 
 import { configureStore } from "@reduxjs/toolkit"
-import { combineReducers, } from "redux"
+import { applyMiddleware, combineReducers, } from "redux"
 import thunkMiddleware from redux-thunk
 
 
 const reducers = combineReducers({
   //reducer goes here
 })
-
 
 const store = configureStore(reducers, applyMiddleware(thunkMiddleware))
