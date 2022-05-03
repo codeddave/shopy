77bdb9c0ab3c9da74fa2409f77d4f68f5bda8841
codeddave <drizzzydave@gmail.com>
2022-05-03 10:04:21 +0100

    building cart  slice

---
+++ app/redux/cart/cartSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
