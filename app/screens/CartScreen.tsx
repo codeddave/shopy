69e3555dd235ca2d848e16204592c1915b7ff45a
codeddave <drizzzydave@gmail.com>
2022-05-10 01:36:29 +0100

    building CartScreen

--- app/screens/CartScreen.tsx
+++ app/screens/CartScreen.tsx
@@ -1,16 +1,36 @@
+import { Box, VStack, Image, Text } from "native-base";
 import React from "react";
-import { Text, View } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import { useSelector } from "react-redux";
 import { selectCartItems } from "../redux/cart";
 
 const CartScreen = () => {
   const cartItems = useSelector(selectCartItems);
   return (
-    <View>
+    <Box bgColor="gray.200" height="full" paddingTop={1}>
       {cartItems.map((cartItem) => (
-        <Text> {cartItem.name}</Text>
+        <Box
+          padding={3}
+          bgColor="white"
+          display="flex"
+          flexDirection="row"
+          borderBottomWidth={0.17}
+          borderBottomColor="gray.400"
+        >
+          <Image
+            resizeMode="contain"
+            width={100}
+            height={100}
+            source={{ uri: cartItem.image }}
+          />
+
+          <VStack paddingLeft={7}>
+            <Text>{cartItem.name}</Text>
+            <Text>{cartItem.price}</Text>
+          </VStack>
+        </Box>
       ))}
-    </View>
+    </Box>
   );
 };
 
