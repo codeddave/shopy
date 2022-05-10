5a92b2526051435c8acff45d76523a1b2415a25d
codeddave <drizzzydave@gmail.com>
2022-05-11 00:06:44 +0100

    give user feedback when cart is empty

--- app/screens/CartScreen.tsx
+++ app/screens/CartScreen.tsx
@@ -10,39 +10,47 @@ const CartScreen = () => {
 
   return (
     <Box bgColor="gray.200" height="full" paddingTop={1}>
-      {cartItems.map((cartItem) => (
-        <Box
-          padding={3}
-          marginX={2}
-          marginY={0.5}
-          bgColor="white"
-          display="flex"
-          flexDirection="row"
-          borderBottomWidth={0.17}
-          borderBottomColor="gray.400"
-        >
-          <Box width={100} height={100}>
-            <Image resizeMode="contain" source={{ uri: cartItem.image }} />
-          </Box>
+      {cartItems.length ? (
+        <>
+          {cartItems.map((cartItem) => (
+            <Box
+              padding={3}
+              marginX={2}
+              marginY={0.5}
+              bgColor="white"
+              display="flex"
+              flexDirection="row"
+              borderBottomWidth={0.17}
+              borderBottomColor="gray.400"
+            >
+              <Box width={100} height={100}>
+                <Image resizeMode="contain" source={{ uri: cartItem.image }} />
+              </Box>
 
-          <VStack paddingLeft={7}>
-            <Text>{cartItem.name}</Text>
-            <Text paddingTop={2}>{cartItem.price}</Text>
-          </VStack>
+              <VStack paddingLeft={7}>
+                <Text>{cartItem.name}</Text>
+                <Text paddingTop={2}>{cartItem.price}</Text>
+              </VStack>
+            </Box>
+          ))}
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="flex-end"
+            marginTop={4}
+            paddingRight={2}
+          >
+            <Text> Total : ${cartTotal}</Text>
+          </Box>
+          <Button colorScheme="green" marginX={2} marginTop={5}>
+            Checkout
+          </Button>
+        </>
+      ) : (
+        <Box height="full" justifyContent="center" alignItems="center">
+          <Text> You have no items in your cart</Text>
         </Box>
-      ))}
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="flex-end"
-        marginTop={4}
-        paddingRight={2}
-      >
-        <Text> Total : ${cartTotal}</Text>
-      </Box>
-      <Button colorScheme="green" marginX={2} marginTop={5}>
-        Checkout
-      </Button>
+      )}
     </Box>
   );
 };
