fc7081aeb50c645d72dbb5d6851e43f8eb1cc8a7
codeddave <drizzzydave@gmail.com>
2022-05-10 01:08:07 +0100

    added redux store to App

--- App.tsx
+++ App.tsx
@@ -3,13 +3,17 @@ import Header from "./app/components/shared/Header";
 import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigator } from "./app/navigation";
+import { Provider } from "react-redux";
+import store from "./app/redux/store";
 export default function App() {
   return (
     <NativeBaseProvider>
-      <NavigationContainer>
-        <Header />
-        <AppNavigator />
-      </NavigationContainer>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Header />
+          <AppNavigator />
+        </NavigationContainer>
+      </Provider>
     </NativeBaseProvider>
   );
 }
