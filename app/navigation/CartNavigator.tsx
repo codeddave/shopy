ccc7deb0cf17e9fa7db5fc5debb33e15be68d110
codeddave <drizzzydave@gmail.com>
2022-05-11 11:57:33 +0100

    created CartNavigator

---
+++ app/navigation/CartNavigator.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { CartScreen, CheckoutScreen } from "../screens";
+
+const Stack = createStackNavigator();
+const CartNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={CartScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={CheckoutScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default CartNavigator;
