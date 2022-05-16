d8da97d45e936139abbfbde1b76f1c3bc66468cb
codeddave <drizzzydave@gmail.com>
2022-05-16 03:46:00 +0100

    update

--- app/components/shared/form /CustomTextInput.tsx
+++ app/components/shared/form /CustomTextInput.tsx
@@ -1,17 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { FC } from "react";
 
 type Props = {
   icon?: string;
+  width?: string;
+  otherProps: any;
 };
-const CustomTextInput = () => {
+const CustomTextInput: FC<Props> = ({
+  width = "100%",
+  icon,
+  ...otherProps
+}) => {
   return (
-    <View>
-      <Text>CustomTextInput</Text>
-    </View>
+    <SafeAreaView>
+      <View style={[styles.container, { width }]}></View>
+    </SafeAreaView>
   );
 };
 
 export default CustomTextInput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    //  backgroundColor: colors.lightGrey,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  textInput: {
+    //  ...defaultStyles.text,
+    width: "100%",
+    // color: colors.mediumGrey,
+  },
+});
