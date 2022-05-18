e589ef910ca5c364687b76f862398269b0b2d546
codeddave <drizzzydave@gmail.com>
2022-05-18 01:35:52 +0100

    updates to CheckoutNavigator

--- app/navigation/CheckoutNavigator.tsx
+++ app/navigation/CheckoutNavigator.tsx
@@ -7,9 +7,9 @@ const Tab = createMaterialTopTabNavigator();
 const CheckoutNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Checkout" component={CheckoutScreen} />
-      <Tab.Screen name="Confirm" component={ConfirmScreen} />
+      <Tab.Screen name="Shipping" component={CheckoutScreen} />
       <Tab.Screen name="Payment" component={PaymentScreen} />
+      <Tab.Screen name="Confirm" component={ConfirmScreen} />
     </Tab.Navigator>
   );
 };
