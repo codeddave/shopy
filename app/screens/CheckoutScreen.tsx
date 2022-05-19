ca82a4e48dacf723568b9fb30fc8d4009298bcb2
codeddave <drizzzydave@gmail.com>
2022-05-19 01:51:48 +0100

    added keyboard aware view

--- app/screens/CheckoutScreen.tsx
+++ app/screens/CheckoutScreen.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { CustomForm, FormField, SubmitButton } from "../components";
-import { Box, Select, Text } from "native-base";
+import { Box, KeyboardAvoidingView, Select, Text } from "native-base";
 import countriesData from "../../assets/data/countries.json";
 import { NavigationScreenProp } from "react-navigation";
 import { FormikValues } from "formik";
+import { Platform } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 type Props = {
   navigation: NavigationScreenProp<any, any>;
@@ -26,65 +28,71 @@ const CheckoutScreen = ({ navigation }: Props) => {
     navigation.navigate("Payment", { order });
   };
   return (
-    <Box
-      width="92%"
-      marginX="auto"
-      justifyContent="center"
-      display="flex"
-      alignItems="center"
+    <KeyboardAwareScrollView
+      viewIsInsideTabBar
+      extraHeight={200}
+      enableOnAndroid
     >
-      <Text marginTop={5} marginBottom={2} fontSize="md">
-        Shipping Address{" "}
-      </Text>
-      <CustomForm
-        onSubmit={(values) => handleCheckout(values)}
-        initialValues={initialValues}
-        //validationSchema={{}}
+      <Box
+        width="92%"
+        marginX="auto"
+        justifyContent="center"
+        display="flex"
+        alignItems="center"
       >
-        <FormField
-          name="phone"
-          {...{
-            placeholder: "Phone",
-            keyboardType: "numeric",
-          }}
-        />
-        <FormField
-          name="shippingAddressOne"
-          {...{ placeholder: "Shipping Address 1" }}
-        />
-        <FormField
-          name="shippingAddressTwo"
-          {...{ placeholder: "Shipping Address 2" }}
-        />
-        <FormField name="city" {...{ placeholder: "City" }} />
-        <FormField
-          name="zipCode"
-          {...{
-            placeholder: "Zip Code",
-            keyboardType: "numeric",
-          }}
-        />
-        <Select
-          bgColor="white"
-          borderRadius={30}
-          placeholder="Select your country"
-          placeholderTextColor="#adadaf"
-          height="10"
-          fontSize="sm"
-          width="full"
-          mt={1}
+        <Text marginTop={5} marginBottom={2} fontSize="md">
+          Shipping Address{" "}
+        </Text>
+        <CustomForm
+          onSubmit={(values) => handleCheckout(values)}
+          initialValues={initialValues}
+          //validationSchema={{}}
         >
-          {countriesData.map((country) => (
-            <Select.Item
-              key={country.code}
-              label={country.name}
-              value={country.name}
-            />
-          ))}
-        </Select>
-        <SubmitButton title="Checkout" />
-      </CustomForm>
-    </Box>
+          <FormField
+            name="phone"
+            {...{
+              placeholder: "Phone",
+              keyboardType: "numeric",
+            }}
+          />
+          <FormField
+            name="shippingAddressOne"
+            {...{ placeholder: "Shipping Address 1" }}
+          />
+          <FormField
+            name="shippingAddressTwo"
+            {...{ placeholder: "Shipping Address 2" }}
+          />
+          <FormField name="city" {...{ placeholder: "City" }} />
+          <FormField
+            name="zipCode"
+            {...{
+              placeholder: "Zip Code",
+              keyboardType: "numeric",
+            }}
+          />
+          <Select
+            bgColor="white"
+            borderRadius={30}
+            placeholder="Select your country"
+            placeholderTextColor="#adadaf"
+            height="10"
+            fontSize="sm"
+            width="full"
+            mt={1}
+          >
+            {countriesData.map((country) => (
+              <Select.Item
+                key={country.code}
+                label={country.name}
+                value={country.name}
+              />
+            ))}
+          </Select>
+          <SubmitButton title="Checkout" />
+        </CustomForm>
+      </Box>
+    </KeyboardAwareScrollView>
   );
 };
 
