6b97edc5bf429610c2bd20978de5b9cda87f9147
codeddave <drizzzydave@gmail.com>
2022-05-19 01:21:28 +0100

    built custom submit button

--- app/components/shared/form /index.ts
+++ app/components/shared/form /index.ts
@@ -1,3 +1,4 @@
 export { default as CustomForm } from "./CustomForm";
 export { default as CustomTextInput } from "./CustomTextInput";
 export { default as FormField } from "./FormField";
+export { default as SubmitButton } from "./SubmitButton";
