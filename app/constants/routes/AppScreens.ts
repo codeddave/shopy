dfc972e804fc89c50176584ca6c30c77153ef80a
codeddave <drizzzydave@gmail.com>
2022-05-22 13:38:07 +0100

    updated screens

---
+++ app/constants/routes/AppScreens.ts
@@ -0,0 +1,12 @@
+export const SCREENS = {
+  HOME: "Home",
+  PRODUCT_DETAIL: "Product Detail",
+  CART: "Cart",
+  ADMIN: "Admin",
+  USER: "User",
+  CART_SCREEN: "CartScreen",
+  CHECKOUT: "Checkout",
+  SHIPPING: "Shipping",
+  CONFIRM: "Confirm",
+  PAYMENT: "Payment",
+};
