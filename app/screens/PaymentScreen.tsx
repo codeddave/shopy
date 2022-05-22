5a86358765e1c632f0f7463d63ce20ae87a9dbcf
codeddave <drizzzydave@gmail.com>
2022-05-22 23:48:51 +0100

    update to Payment Screen

--- app/screens/PaymentScreen.tsx
+++ app/screens/PaymentScreen.tsx
@@ -1,6 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+type PaymentMethodsType = {
+  name: string;
+  value: number;
+};
+const paymentMethods: PaymentMethodsType[] = [
+  { name: "Cash on Delivery", value: 1 },
+  { name: "Cash on mDelivery", value: 1 },
+];
 const Payment = () => {
   return (
     <View>
