4108061b7c1c07a267526a1143328fda5f322690
codeddave <drizzzydave@gmail.com>
2022-05-23 11:38:09 +0100

    updated typing for Payment Screen

--- app/screens/PaymentScreen.tsx
+++ app/screens/PaymentScreen.tsx
@@ -1,13 +1,22 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import type { StackScreenProps } from "@react-navigation/stack";
 
+type RootStackParamList = {
+  Shipping: undefined;
+  Payment: { order: any };
+  Confirm: any /* { sort: 'latest' | 'top' } | undefined; */;
+};
+
+type ScreenProps = StackScreenProps<RootStackParamList, "Payment">;
 type PaymentOptionsType = {
   name: string;
   value: number;
 };
 const paymentMethods: PaymentOptionsType[] = [
   { name: "Cash on Delivery", value: 1 },
-  { name: "Cash on Delivery", value: 1 },
+  { name: "Bank Transfer", value: 2 },
+  { name: "Card Payment", value: 3 },
 ];
 
 const paymentCardOptions: PaymentOptionsType[] = [
@@ -16,7 +25,11 @@ const paymentCardOptions: PaymentOptionsType[] = [
   { name: "MasterCard", value: 3 },
   { name: "Other", value: 4 },
 ];
-const Payment = () => {
+const Payment = ({ route }: ScreenProps) => {
+  const order = route.params.order;
+  const [selected, setSelected] = useState();
+  const [card, setCard] = useState();
+
   return (
     <View>
       <Text>Payment</Text>
