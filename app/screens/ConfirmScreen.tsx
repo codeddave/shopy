cdf227970b8508e38c55451abdd1a023c8576331
codeddave <drizzzydave@gmail.com>
2022-05-25 01:30:57 +0100

    updates to confirm screen

--- app/screens/ConfirmScreen.tsx
+++ app/screens/ConfirmScreen.tsx
@@ -1,6 +1,6 @@
 import { View } from "react-native";
 import React from "react";
-import { Text } from "native-base";
+import { Box, Text } from "native-base";
 import { StackScreenProps } from "@react-navigation/stack";
 
 type RootStackParamList = {
@@ -11,13 +11,16 @@ type RootStackParamList = {
 
 type ScreenProps = StackScreenProps<RootStackParamList, "Confirm">;
 
-const Confirm = ({}: ScreenProps) => {
+const Confirm = ({ route }: ScreenProps) => {
+  const order = route.params?.order;
   return (
     <View>
       <Text mt="5" mb="2" textAlign="center" fontSize="md" fontWeight="bold">
         Confirm Order
       </Text>
-      <Text>Confirm</Text>
+      <Box textAlign="center" display="flex" alignItems="center">
+        <Text>Address: {order?.shippingAddressOne}</Text>
+      </Box>
     </View>
   );
 };
