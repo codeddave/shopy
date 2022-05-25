714c8ef62a721f9b8afd523d2fb594d0bda23354
codeddave <drizzzydave@gmail.com>
2022-05-25 01:16:49 +0100

    building confirm screen

--- app/screens/ConfirmScreen.tsx
+++ app/screens/ConfirmScreen.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
+import { Text } from "native-base";
+import { StackScreenProps } from "@react-navigation/stack";
 
-const Confirm = () => {
+type RootStackParamList = {
+  Shipping: undefined;
+  Payment: { order: any };
+  Confirm: { order: any };
+};
+
+type ScreenProps = StackScreenProps<RootStackParamList, "Confirm">;
+
+const Confirm = ({}: ScreenProps) => {
   return (
     <View>
+      <Text mt="5" mb="2" textAlign="center" fontSize="md" fontWeight="bold">
+        Confirm Order
+      </Text>
       <Text>Confirm</Text>
     </View>
   );
