7a3eba5b552e7cf83aa27cc319eae57e3002e319
codeddave <drizzzydave@gmail.com>
2022-05-28 02:40:35 +0100

    created product Slice, added to root reducer

--- app/redux/rootReducer.ts
+++ app/redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { cartReducer } from "./cart/cartSlice";
+import { productReducer } from "./products/productSlice";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
+  product: productReducer,
 });
 
 export default rootReducer;
