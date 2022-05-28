ebe19edd4b86ce30a56848ea55198185946a26cb
codeddave <drizzzydave@gmail.com>
2022-05-28 12:08:15 +0100

    bug fix

--- app/api/products.ts
+++ app/api/products.ts
@@ -2,9 +2,9 @@ import { UNAUTHENTICATED_ROUTES } from "./apiRoutes";
 import client from "./config";
 
 const getProducts = () => {
-  client.get(UNAUTHENTICATED_ROUTES.ALL_PRODUCTS);
+  return client.get(UNAUTHENTICATED_ROUTES.ALL_PRODUCTS);
 };
 
-export const products = {
+export const productApi = {
   getProducts,
 };
