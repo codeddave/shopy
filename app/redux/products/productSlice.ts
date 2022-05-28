2483a160771894c03c6db3e4c35ccc7eb163c396
codeddave <drizzzydave@gmail.com>
2022-05-28 12:31:53 +0100

    updated Product Slice

--- app/redux/products/productSlice.ts
+++ app/redux/products/productSlice.ts
@@ -1,10 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { productApi } from "../../api";
 
+import { SliceStatus } from "../../interfaces";
+
+const getProducts = createAsyncThunk<any>("products/getProducts", async () => {
+  const response = await productApi.getProducts();
+  return response;
+});
 type ProductState = {
   products: any[];
+  isLoading: SliceStatus;
+  error?: string;
 };
 const initialState: ProductState = {
   products: [],
+  isLoading: SliceStatus.idle,
+  error: "",
 };
 
 const productSlice = createSlice({
@@ -15,9 +26,20 @@ const productSlice = createSlice({
       state.products.push(action.payload);
     },
   },
-  /*   extraReducers: (builder) => {    
- 
-  }, */
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = SliceStatus.pending;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = SliceStatus.resolved;
+        state.products = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = SliceStatus.rejected;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export const { reducer: productReducer, name: productReducerName } =
