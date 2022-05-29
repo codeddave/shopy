1e0d4c7f35f9131bc32bcc446d9f0c01a1cf7d02
codeddave <drizzzydave@gmail.com>
2022-05-29 13:37:47 +0100

    useFetchProducts hook to get data from api, added ActivityIndicator

--- app/screens/ProductScreen.tsx
+++ app/screens/ProductScreen.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
 import data from "../../assets/data/products.json";
 import categoriesData from "../../assets/data/categories.json";
 import { NavigationScreenProp } from "react-navigation";
 import ProductList from "../components/products/ProductList";
-import { Input, Icon, ScrollView, Text } from "native-base";
+import { Input, Icon, ScrollView, Text, Box, VStack } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import SearchedProductsScreen from "./SearchedProductsScreen";
 import Banner from "../components/shared/Banner";
 import { Categories } from "../components";
+import { useFetchProducts } from "../components/hooks/apiHooks/useFetchProducts";
+import { SliceStatus } from "../interfaces";
 
 //import { StackScreenProps} from "@react-navigation/stack";
 
@@ -17,15 +19,17 @@ type Props = {
 };
 const ProductScreen = ({ navigation }: Props) => {
   const [filteredProducts, setFilteredProducts] = useState<{}[]>([]);
-  const [products, setProducts] = useState<{}[]>([]);
-  const [categories, setCategories] = useState<{}[]>([]);
-  const [productCategories, setProductCategories] = useState<{}[]>([]);
+  //const [products, setProducts] = useState<{}[]>([]);
 
   const [isActive, setIsActive] = useState<number>(-1);
   const [initialState, setInitialState] = useState<{}[]>([]);
 
   const [focus, setFocus] = useState(false);
-  useEffect(() => {
+
+  const { products, productCategories, categories, isProductsLoading } =
+    useFetchProducts();
+  //console.log(products);
+  /*   useEffect(() => {
     setProducts(data);
     setFilteredProducts(data);
     setInitialState(data);
@@ -37,7 +41,7 @@ const ProductScreen = ({ navigation }: Props) => {
       setFilteredProducts([]);
       setCategories([]);
     };
-  }, []);
+  }, []); */
   const searchProducts = (text: string) => {
     text === "" ? setFocus(false) : setFocus(true);
     setFilteredProducts(
@@ -71,79 +75,87 @@ const ProductScreen = ({ navigation }: Props) => {
 
   return (
     <>
-      <Input
-        placeholder="Search"
-        width="100%"
-        variant="filled"
-        backgroundColor="whitesmoke"
-        borderRadius="10"
-        onFocus={openList}
-        onChangeText={(text) => searchProducts(text)}
-        py="1"
-        px="2"
-        borderWidth="0"
-        InputLeftElement={
-          <Icon
-            ml="2"
-            size="4"
-            color="gray.600"
-            as={<MaterialIcons name="search" />}
-          />
-        }
-        InputRightElement={
-          focus ? (
-            <Icon
-              mr="2"
-              size="3"
-              color="gray.600"
-              as={<MaterialIcons name="close" onPress={closeList} />}
-            />
-          ) : null
-        }
-      />
-      {focus ? (
-        <SearchedProductsScreen
-          navigation={navigation}
-          filteredProducts={filteredProducts}
-        />
+      {isProductsLoading === SliceStatus.pending ? (
+        <VStack alignItems="center" justifyContent="center" height="100%">
+          <ActivityIndicator />
+        </VStack>
       ) : (
-        <ScrollView
-          style={[
-            {
-              backgroundColor: "gainsboro",
-              paddingBottom: 30,
-              marginTop: 4,
-            },
-          ]}
-        >
-          <View>
-            <Banner />
-          </View>
-          <Categories
-            categories={categories}
-            categoryFilter={changeCategory}
-            productCategories={productCategories}
-            active={isActive}
-            setActive={setIsActive}
-          />
-          {/*  {productCategories} */}
-
-          {productCategories.length > 0 ? (
-            <View style={styles.container}>
-              {productCategories.map((item: any) => (
-                <ProductList
-                  key={item.id}
-                  item={item}
-                  navigation={navigation}
+        <>
+          <Input
+            placeholder="Search"
+            width="100%"
+            variant="filled"
+            backgroundColor="whitesmoke"
+            borderRadius="10"
+            onFocus={openList}
+            onChangeText={(text) => searchProducts(text)}
+            py="1"
+            px="2"
+            borderWidth="0"
+            InputLeftElement={
+              <Icon
+                ml="2"
+                size="4"
+                color="gray.600"
+                as={<MaterialIcons name="search" />}
+              />
+            }
+            InputRightElement={
+              focus ? (
+                <Icon
+                  mr="2"
+                  size="3"
+                  color="gray.600"
+                  as={<MaterialIcons name="close" onPress={closeList} />}
                 />
-              ))}
-            </View>
+              ) : null
+            }
+          />
+          {focus ? (
+            <SearchedProductsScreen
+              navigation={navigation}
+              filteredProducts={filteredProducts}
+            />
           ) : (
-            <Text textAlign="center" paddingTop={4} fontSize="lg">
-              Product Not Found
-            </Text>
+            <ScrollView
+              style={[
+                {
+                  backgroundColor: "gainsboro",
+                  paddingBottom: 30,
+                  marginTop: 4,
+                },
+              ]}
+            >
+              <View>
+                <Banner />
+              </View>
+              <Categories
+                categories={categories}
+                categoryFilter={changeCategory}
+                productCategories={productCategories}
+                active={isActive}
+                setActive={setIsActive}
+              />
+              {/*  {productCategories} */}
+
+              {productCategories.length > 0 ? (
+                <View style={styles.container}>
+                  {productCategories.map((item: any) => (
+                    <ProductList
+                      key={item.id}
+                      item={item}
+                      navigation={navigation}
+                    />
+                  ))}
+                </View>
+              ) : (
+                <Text textAlign="center" paddingTop={4} fontSize="lg">
+                  Product Not Found
+                </Text>
+              )}
+            </ScrollView>
           )}
-        </ScrollView>
+        </>
       )}
     </>
   );
