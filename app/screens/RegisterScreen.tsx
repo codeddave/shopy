04b5737659bc4aab87a6d9f0811c64280c42bc42
codeddave <drizzzydave@gmail.com>
2022-06-05 00:10:26 +0100

    added Keyboard aware scroll view

--- app/screens/RegisterScreen.tsx
+++ app/screens/RegisterScreen.tsx
@@ -3,6 +3,7 @@ import { CustomForm, FormField, SubmitButton } from "../components";
 import { Box, Text } from "native-base";
 import { StackScreenProps } from "@react-navigation/stack";
 import { SCREENS } from "../constants/routes/AppScreens";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 type RootStackParamList = {
   Login: undefined;
@@ -13,55 +14,60 @@ type ScreenProps = StackScreenProps<RootStackParamList, "Register">;
 
 const RegisterScreen = ({ navigation }: ScreenProps) => {
   return (
-    <Box pt="24" width="94%" marginX="auto">
-      <CustomForm
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        onSubmit={() => console.log("hello")}
-      >
-        <Text textAlign="center" marginBottom={2} fontSize="3xl">
-          Register
-        </Text>
-        <FormField
-          name="name"
-          {...{
-            placeholder: "Full Name",
-            autoCorrect: false,
-            autoCapitalize: "none",
-            icon: "account",
+    <KeyboardAwareScrollView enableOnAndroid>
+      <Box pt="24" width="94%" marginX="auto">
+        <CustomForm
+          initialValues={{
+            email: "",
+            password: "",
           }}
-        />
-        <FormField
-          name="email"
-          {...{
-            placeholder: "Email",
-            keyboardType: "email-address",
-            autoCapitalize: "none",
-            icon: "email",
-            textContentType: "emailAddress",
-          }}
-        />
+          onSubmit={() => console.log("hello")}
+        >
+          <Text textAlign="center" marginTop={4} mb="6" fontSize="3xl">
+            Register
+          </Text>
+          <FormField
+            name="name"
+            {...{
+              placeholder: "Full Name",
+              autoCorrect: false,
+              autoCapitalize: "none",
+              icon: "account",
+            }}
+          />
+          <FormField
+            name="email"
+            {...{
+              placeholder: "Email",
+              keyboardType: "email-address",
+              autoCapitalize: "none",
+              icon: "email",
+              textContentType: "emailAddress",
+            }}
+          />
 
-        <FormField
-          name="password"
-          {...{
-            placeholder: "Password",
-            secureTextEntry: true,
-            autoCapitalize: "none",
-            autoCorrect: false,
-            icon: "lock",
-            textContentType: "password",
-          }}
-        />
-        <Text onPress={() => navigation.navigate(SCREENS.LOGIN as any)} pt="2">
-          Already have an account?
-        </Text>
+          <FormField
+            name="password"
+            {...{
+              placeholder: "Password",
+              secureTextEntry: true,
+              autoCapitalize: "none",
+              autoCorrect: false,
+              icon: "lock",
+              textContentType: "password",
+            }}
+          />
+          <Text
+            onPress={() => navigation.navigate(SCREENS.LOGIN as any)}
+            pt="2"
+          >
+            Already have an account?
+          </Text>
 
-        <SubmitButton title="Register" />
-      </CustomForm>
-    </Box>
+          <SubmitButton title="Register" />
+        </CustomForm>
+      </Box>
+    </KeyboardAwareScrollView>
   );
 };
 
