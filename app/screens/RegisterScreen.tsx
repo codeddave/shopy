d12d19a694fed3473c8afeaf319f727635cfca9e
codeddave <drizzzydave@gmail.com>
2022-06-04 23:39:02 +0100

    build register screen

--- app/screens/RegisterScreen.tsx
+++ app/screens/RegisterScreen.tsx
@@ -1,11 +1,56 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { CustomForm, FormField, SubmitButton } from "../components";
+import { Box, Text } from "native-base";
 
 const RegisterScreen = () => {
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <Box pt="20" width="95%" marginX="auto">
+      <CustomForm
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={() => console.log("hello")}
+      >
+        <Text textAlign="center" marginBottom={2} fontSize="3xl">
+          Register
+        </Text>
+        <FormField
+          name="name"
+          {...{
+            placeholder: "Full Name",
+            autoCorrect: false,
+            autoCapitalize: "none",
+            icon: "account",
+          }}
+        />
+        <FormField
+          name="email"
+          {...{
+            placeholder: "Email",
+            keyboardType: "email-address",
+            autoCapitalize: "none",
+            icon: "email",
+            textContentType: "emailAddress",
+          }}
+        />
+
+        <FormField
+          name="password"
+          {...{
+            placeholder: "Password",
+            secureTextEntry: true,
+            autoCapitalize: "none",
+            autoCorrect: false,
+            icon: "lock",
+            textContentType: "password",
+          }}
+        />
+        <Text pt="2">Forgot Password?</Text>
+
+        <SubmitButton title="Log In" />
+      </CustomForm>
+    </Box>
   );
 };
 
