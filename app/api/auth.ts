d57c2a7193bb1bf1f6ce8e5f1b36e9bb522de86e
codeddave <drizzzydave@gmail.com>
2022-06-05 21:12:12 +0100

    setting up auth apis

---
+++ app/api/auth.ts
@@ -0,0 +1,24 @@
+import { AUTH_ROUTES } from "./apiRoutes";
+import { create } from "apisauce";
+/* const apiClient = create({
+  baseURL: "https://wander-lust-mern.herokuapp.com",
+});
+ */
+const apiClient = create({
+  baseURL: "https://let-itgo.herokuapp.com",
+});
+export const logIn = async (loginData: { email: string; password: string }) => {
+  return apiClient.post(AUTH_ROUTES.LOGIN, loginData);
+};
+
+export const register = async (registerData: {
+  email: string;
+  password: string;
+  fullName: string;
+}) => {
+  return apiClient.post(AUTH_ROUTES.REGISTER, registerData);
+};
+
+/* export const forgotPassword = async (email) => {
+  return apiClient.post("/user/forgot-password", { email });
+}; */
