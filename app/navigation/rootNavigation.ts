24ae1c17938179d6141b672a3c6725e8c11e2fef
codeddave <drizzzydave@gmail.com>
2022-06-12 00:21:42 +0100

    created root navigation

---
+++ app/navigation/rootNavigation.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const navigationRef = React.createRef();
+
+export const navigate = (name: any, params?: any) =>
+  (navigationRef as any).current?.navigate(name, params);
