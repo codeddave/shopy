fcb47485a8842d6aabfd7bc58c9b5f9a3a71ea6c
codeddave <drizzzydave@gmail.com>
2022-06-12 00:24:57 +0100

    added navigation ref

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@
 import Header from "./app/components/shared/Header";
 import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
-import { AppNavigator } from "./app/navigation";
+import { AppNavigator, navigationRef } from "./app/navigation";
 import { Provider } from "react-redux";
 import store from "./app/redux/store";
 export default function App() {
   return (
     <NativeBaseProvider>
       <Provider store={store}>
-        <NavigationContainer>
+        <NavigationContainer ref={navigationRef as any}>
           <Header />
           <AppNavigator />
         </NavigationContainer>
--- app/navigation/index.ts
+++ app/navigation/index.ts
@@ -1,2 +1,3 @@
 export { default as AppNavigator } from "./AppNavigator";
+export * from "./rootNavigation";
 export { default as UserNavigator } from "./UserNavigator";
