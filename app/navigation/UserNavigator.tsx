254d9a7af6566437e6f0bbc535e107965bc12d40
codeddave <drizzzydave@gmail.com>
2022-06-12 14:27:12 +0100

    show user screen if user is logged in and  exists in redux store

--- app/navigation/UserNavigator.tsx
+++ app/navigation/UserNavigator.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { LoginScreen, RegisterScreen } from "../screens";
+import { useSelector } from "react-redux";
+import { selectUser } from "../redux";
+import UserScreen from "../screens/UserScreen";
 const Stack = createStackNavigator();
 const UserNavigator = () => {
+  const user = useSelector(selectUser);
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Register"
-        component={RegisterScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
+      {user ? (
+        <Stack.Screen
+          name="user"
+          component={UserScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      ) : (
+        <>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
