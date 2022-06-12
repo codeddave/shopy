5576145aa961ba82ae8091e4623f1abe3942d348
codeddave <drizzzydave@gmail.com>
2022-06-12 18:31:28 +0100

    fetch user profile hook

---
+++ app/components/hooks/apiHooks/useFetchUserProfile.ts
@@ -0,0 +1,28 @@
+import { View, Text } from "react-native";
+import React, { useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import { getUserById, selectUser, selectUserProfile } from "../../../redux";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../../redux/store";
+import { CurrentUser } from "../../../interfaces";
+
+const useFetchUserProfile = (): {
+  userProfile: CurrentUser | null;
+} => {
+  const user = useSelector(selectUser);
+
+  const userProfile = useSelector(selectUserProfile);
+  const dispatch: AppDispatch = useDispatch();
+
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(getUserById(user!.id));
+    }, [])
+  );
+
+  return {
+    userProfile,
+  };
+};
+
+export default useFetchUserProfile;
