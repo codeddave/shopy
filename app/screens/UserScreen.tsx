06ac241dbe7500c3a52cd10aa2db2f3e7f27583c
codeddave <drizzzydave@gmail.com>
2022-06-14 00:26:00 +0100

    log out fn on user profile page

--- app/screens/UserScreen.tsx
+++ app/screens/UserScreen.tsx
@@ -1,10 +1,20 @@
-import { Text } from "native-base";
+import { Box, Button, Text } from "native-base";
 import React from "react";
+import { useDispatch } from "react-redux";
 import useFetchUserProfile from "../components/hooks/apiHooks/useFetchUserProfile";
+import { logOut } from "../redux";
 
 const UserScreen = () => {
   const { userProfile } = useFetchUserProfile();
-  return <Text>{userProfile?.fullName}</Text>;
+
+  const dispatch = useDispatch();
+
+  return (
+    <Box>
+      <Text>{userProfile?.fullName}</Text>;
+      <Button onPress={() => dispatch(logOut())}>log out</Button>
+    </Box>
+  );
 };
 
 export default UserScreen;
