764554f6334070f5e609295f40100776741c93ee
codeddave <drizzzydave@gmail.com>
2022-06-13 10:13:10 +0100

    setting up redux persist to handle saving user to Asnycstorage

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware, { ThunkMiddleware } from "redux-thunk";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
@@ -11,4 +12,5 @@ const store = configureStore({
       .concat(thunkMiddleware as ThunkMiddleware),
 });
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
 export default store;
