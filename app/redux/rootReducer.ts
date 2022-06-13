c494205ec9c63bec703a934d58b5f117d24d851a
codeddave <drizzzydave@gmail.com>
2022-06-13 10:13:32 +0100

    setting up redux persist to handle saving user to Asnycstorage

--- app/redux/rootReducer.ts
+++ app/redux/rootReducer.ts
@@ -3,13 +3,20 @@ import { cartReducer } from "./cart/cartSlice";
 import { categoryReducer } from "./categories/categorySlice";
 import { productReducer } from "./products/productSlice";
 import { userReducer } from "./user";
+import { persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["cart", "user"],
+};
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   product: productReducer,
   category: categoryReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
 
-export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>;
