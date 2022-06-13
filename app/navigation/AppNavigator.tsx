d1beecd2887d314d6bd6452a3a47931fa4ed54ad
codeddave <drizzzydave@gmail.com>
2022-06-14 00:25:02 +0100

    wrote logic to logut user when token expires

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeNavigator from "./HomeNavigator";
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import CartIcon from "../components/cart/CartIcon";
 import CartNavigator from "./CartNavigator";
 import UserNavigator from "./UserNavigator";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut, selectUserToken } from "../redux";
+import decode from "jwt-decode";
+
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
+  const userToken = useSelector(selectUserToken);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    Alert.alert("hi!!!");
+    if (userToken) {
+      const decodedToken = decode(userToken) as any;
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        dispatch(logOut());
+      }
+    }
+  }, []);
   return (
     <Tab.Navigator>
       <Tab.Screen
