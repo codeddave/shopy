cabc793609a0738237c72659020c04c76d26ca7f
codeddave <drizzzydave@gmail.com>
2022-06-18 01:25:04 +0100

    creating Admin Screens

---
+++ app/screens/admin/AdminProductsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AdminProductsScreen = () => {
+  return (
+    <View>
+      <Text>AdminProductsScreen</Text>
+    </View>
+  );
+};
+
+export default AdminProductsScreen;
