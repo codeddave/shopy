90fc70d19021682b7411543c7c4184f3f0472c4c
codeddave <drizzzydave@gmail.com>
2022-06-18 16:16:11 +0100

    updates to the AdminNavigagtor

--- app/navigation/AdminNavigator.tsx
+++ app/navigation/AdminNavigator.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { AdminOrdersScreen } from "../screens";
+import AdminCategoriesScreen from "../screens/admin/AdminCategoriesScreen";
+import AdminProductsScreen from "../screens/admin/AdminProductsScreen";
 
 const Stack = createStackNavigator();
 const AdminNavigator = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen
+        name="AdminProductsScreen"
+        component={AdminProductsScreen}
+        options={{
+          title: "Products",
+        }}
+      />
       <Stack.Screen name="AdminOrdersScreen" component={AdminOrdersScreen} />
+
+      <Stack.Screen name="AdminOrdersScreen" component={AdminOrdersScreen} />
+      <Stack.Screen
+        name="AdmincategoriesScreen"
+        component={AdminCategoriesScreen}
+      />
     </Stack.Navigator>
   );
 };
