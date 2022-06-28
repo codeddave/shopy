c1f5d5f848b26c3df934e49da91b1f383a09a1a9
codeddave <drizzzydave@gmail.com>
2022-06-28 23:06:06 +0100

    building Admin Products screen

--- app/screens/admin/AdminProductsScreen.tsx
+++ app/screens/admin/AdminProductsScreen.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native";
 import React from "react";
+import {
+  CustomActivityIndicator,
+  SearchInput,
+  useFetchProducts,
+  useProducts,
+} from "../../components";
+import { Box } from "native-base";
+import { SliceStatus } from "../../interfaces";
 
 const AdminProductsScreen = () => {
+  const { products, isProductsLoading } = useFetchProducts();
+  const { openList, closeList, focus } = useProducts();
   return (
-    <View>
-      <Text>AdminProductsScreen</Text>
-    </View>
+    <Box>
+      {isProductsLoading === SliceStatus.pending ? (
+        <CustomActivityIndicator />
+      ) : (
+        <SearchInput
+          onFocus={openList}
+          closeList={closeList}
+          searchProducts={() => console.log("khdv")}
+          focus={focus}
+        />
+      )}
+    </Box>
   );
 };
 
