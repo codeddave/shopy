9b89eac685ad6f581797a2725888ebe5650955af
codeddave <drizzzydave@gmail.com>
2022-07-02 23:14:44 +0100

    building List Item

--- app/screens/admin/AdminProductsScreen.tsx
+++ app/screens/admin/AdminProductsScreen.tsx
@@ -5,9 +5,10 @@ import {
   useFetchProducts,
   useProducts,
 } from "../../components";
-import { Box, Text } from "native-base";
+import { Box, Image, Text } from "native-base";
 import { SliceStatus } from "../../interfaces";
 import { FlatList } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const AdminProductsScreen = () => {
   const { products, isProductsLoading } = useFetchProducts();
@@ -26,7 +27,19 @@ const AdminProductsScreen = () => {
           />
           <FlatList
             data={products}
-            renderItem={({ item }) => <Text>{item.name}</Text>}
+            renderItem={({ item }) => (
+              <Box>
+                <TouchableOpacity>
+                  <Image source={{ uri: item.image ? item.image : "" }} />
+
+                  <Text> {item.brand}</Text>
+                  <Text numberOfLines={1} ellipsizeMode="tail">
+                    {" "}
+                    {item.name}
+                  </Text>
+                </TouchableOpacity>
+              </Box>
+            )}
             keyExtractor={(item) => item.id}
           />
           {/*  {products.map((product) => (
