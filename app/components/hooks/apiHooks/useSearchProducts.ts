7ec49798bed9fc109c067ff7222187a1c6e87d5e
codeddave <drizzzydave@gmail.com>
2022-07-04 01:41:14 +0100

    handle search Products display

--- app/components/hooks/apiHooks/useSearchProducts.ts
+++ app/components/hooks/apiHooks/useSearchProducts.ts
@@ -2,18 +2,25 @@ import { useDispatch } from "react-redux";
 import { searchProducts } from "../../../redux";
 import { AppDispatch } from "../../../redux/store";
 import { debounce } from "lodash";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 export const useSearchProducts = () => {
+  const [showSearchProducts, setShowSearchProducts] = useState(false);
   const dispatch: AppDispatch = useDispatch();
 
   const handleSearchProducts = useCallback(
     debounce((searchTerm: string) => {
+      if (searchTerm === "") {
+        setShowSearchProducts(false);
+      } else {
+        setShowSearchProducts(true);
+      }
       dispatch(searchProducts(searchTerm));
     }, 1000),
     []
   );
   return {
     handleSearchProducts,
+    showSearchProducts,
   };
 };
