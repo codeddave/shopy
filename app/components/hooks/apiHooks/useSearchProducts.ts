b0574a797c92d53f4f927b8626dd944b48367154
codeddave <drizzzydave@gmail.com>
2022-07-05 02:45:47 +0100

    updated useSearchProducts hook

--- app/components/hooks/apiHooks/useSearchProducts.ts
+++ app/components/hooks/apiHooks/useSearchProducts.ts
@@ -1,5 +1,9 @@
-import { useDispatch } from "react-redux";
-import { searchProducts } from "../../../redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  searchProducts,
+  selectSearchedProducts,
+  selectSearchedProductsLoading,
+} from "../../../redux";
 import { AppDispatch } from "../../../redux/store";
 import { debounce } from "lodash";
 import { useCallback, useState } from "react";
@@ -7,6 +11,8 @@ import { useCallback, useState } from "react";
 export const useSearchProducts = () => {
   const [showSearchProducts, setShowSearchProducts] = useState(false);
   const dispatch: AppDispatch = useDispatch();
+  const searchedProducts = useSelector(selectSearchedProducts);
+  const isLoadingSearchedProducts = useSelector(selectSearchedProductsLoading);
 
   const handleSearchProducts = useCallback(
     debounce((searchTerm: string) => {
@@ -22,5 +28,7 @@ export const useSearchProducts = () => {
   return {
     handleSearchProducts,
     showSearchProducts,
+    searchedProducts,
+    isLoadingSearchedProducts,
   };
 };
