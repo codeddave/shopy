5bf41b5dbc794f7e39462156b58d50286cd6c0bf
codeddave <drizzzydave@gmail.com>
2022-07-27 00:20:32 +0100

    building Modal for the List Item

--- app/components/admin/ListItem.tsx
+++ app/components/admin/ListItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity, Modal } from "react-native";
 import React, { FC } from "react";
 import { Box, Image, Text } from "native-base";
+import { useToggle } from "../hooks";
 
 type ProductData = {
   name: string;
@@ -20,37 +21,42 @@ type Props = {
   item: ProductData;
 };
 export const ListItem: FC<Props> = ({ item }) => {
+  const { toggleOn: modalVisible, onToggleClick: toggleModalVisible } =
+    useToggle();
   return (
-    <TouchableOpacity style={styles.container}>
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Box w="1/6" h="10">
-          <Image
-            source={{
-              uri: "https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png",
-            }}
-            resizeMode="contain"
-            w="full"
-            h="full"
-          />
-        </Box>
+    <Box>
+      <Modal animationType="fade" transparent visible={modalVisible}></Modal>
+      <TouchableOpacity style={styles.container}>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box w="1/6" h="10">
+            <Image
+              source={{
+                uri: "https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png",
+              }}
+              resizeMode="contain"
+              w="full"
+              h="full"
+            />
+          </Box>
 
-        <Text numberOfLines={1} ellipsizeMode="tail" w="1/6">
-          {item.brand}
-        </Text>
-        <Text w="1/6" numberOfLines={1} ellipsizeMode="tail">
-          {item.name}
-        </Text>
-        <Text w="1/6" numberOfLines={1} ellipsizeMode="tail">
-          {item.category.name}
-        </Text>
-        <Text w="1/6"> ${item.price}</Text>
-      </Box>
-    </TouchableOpacity>
+          <Text numberOfLines={1} ellipsizeMode="tail" w="1/6">
+            {item.brand}
+          </Text>
+          <Text w="1/6" numberOfLines={1} ellipsizeMode="tail">
+            {item.name}
+          </Text>
+          <Text w="1/6" numberOfLines={1} ellipsizeMode="tail">
+            {item.category.name}
+          </Text>
+          <Text w="1/6"> ${item.price}</Text>
+        </Box>
+      </TouchableOpacity>
+    </Box>
   );
 };
 const styles = StyleSheet.create({
